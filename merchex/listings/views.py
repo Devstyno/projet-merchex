82391e4b6299cf1119b0bc682082407f24e99e06
devstyno <ddevstyno@gmail.com>
2024-01-21 16:43:40 +0000

    creation des modeles d'url

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def hello(request):
+    return HttpResponse("<h1>Hello Django!</h1>")
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons Merch !</p>')
+
+def listings(request):
+    return HttpResponse('<h1>Annonces</h1> <p>Restez a l\'ecoute</p>')
+
+def contact_us(request):
+    return HttpResponse('<h1>Contactez-nous</h1> <p>Formulaire bientot disponible</p>')
