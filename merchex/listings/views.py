0795e4821a9815ea99067b727d6658d12ec5ec03
devstyno <ddevstyno@gmail.com>
2024-01-21 22:12:15 +0000

    #

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,14 +1,34 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band, Listing
 
 def hello(request):
-    return HttpResponse("<h1>Hello Django!</h1>")
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Mes groupes préférés sont :<p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>
+""")
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons Merch !</p>')
 
 def listings(request):
-    return HttpResponse('<h1>Annonces</h1> <p>Restez a l\'ecoute</p>')
+    listings = Listing.objects.all()
+    return HttpResponse(f"""
+        <h1>Annonces</h1>
+        <p>Bientot, nous aurons :<p>
+        <ul>
+            <li>{listings[0].title}</li>
+            <li>{listings[1].title}</li>
+            <li>{listings[2].title}</li>
+            <li>{listings[3].title}</li>
+        </ul>
+""")
 
 def contact_us(request):
     return HttpResponse('<h1>Contactez-nous</h1> <p>Formulaire bientot disponible</p>')
