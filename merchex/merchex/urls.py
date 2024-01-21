7a142e95beffadaa2091498595e000b1c7b4f5e4
devstyno <ddevstyno@gmail.com>
2024-01-21 16:43:57 +0000

    creation des modeles d'url

--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from listings import views
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', views.hello),
+    path('about-us/', views.about),
+    path('listings/', views.listings),
+    path('contact-us/', views.contact_us)
 ]
