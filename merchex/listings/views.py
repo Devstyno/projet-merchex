607680ebdd1d7cd04ba83179fb2bda9688c0b011
devstyno <ddevstyno@gmail.com>
2024-01-22 06:22:57 +0000

    #

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -6,21 +6,12 @@ def hello(request):
     bands = Band.objects.all()
     return render(request, "listings/hello.html", {"bands" : bands})
 
-def about_us(request):
-    return HttpResponse('<h1>À propos</h1> <p>Nous adorons Merch !</p>')
-
 def listings(request):
     listings = Listing.objects.all()
-    return HttpResponse(f"""
-        <h1>Annonces</h1>
-        <p>Bientot, nous aurons :<p>
-        <ul>
-            <li>{listings[0].title}</li>
-            <li>{listings[1].title}</li>
-            <li>{listings[2].title}</li>
-            <li>{listings[3].title}</li>
-        </ul>
-""")
+    return render(request, "listings/listings.html", {"listings" : listings})
+
+def about_us(request):
+    return render(request, "listings/about_us.html")
 
 def contact_us(request):
-    return HttpResponse('<h1>Contactez-nous</h1> <p>Formulaire bientot disponible</p>')
+    return render(request, "contact_us.html")
