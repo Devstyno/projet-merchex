d9c8d6c0c9aa333490e8066bebe8b6b3c9d1ddbd
devstyno <ddevstyno@gmail.com>
2024-01-24 15:03:37 +0000

    #

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -3,14 +3,19 @@ from django.core.mail import send_mail
 from listings.models import Band, Listing
 from listings.forms import ContactUsForm, BandForm, ListingForm
 
+# interface crud des modeles
+## band
+### read all
 def band_list(request):
     bands = Band.objects.all()
     return render(request, "listings/bands.html", {"bands" : bands})
 
+### read one
 def band_details(request, id):
     band = Band.objects.get(id=id)
     return render(request, "listings/band_details.html", {"band" : band})
 
+### create
 def band_create(request):
     if request.method == "POST":
         form = BandForm(request.POST)
@@ -21,6 +26,7 @@ def band_create(request):
         form = BandForm()
     return render(request, 'listings/band_create.html', {'form' : form})
 
+### update
 def band_update(request, id):
     band = Band.objects.get(id=id)
     if request.method == "POST":
@@ -32,14 +38,27 @@ def band_update(request, id):
         form = BandForm(instance=band)
     return render(request, 'listings/band_update.html', {'form' : form})
 
+### delete
+def band_delete(request, id):
+    band = Band.objects.get(id=id)
+    if request.method == "POST":
+        band.delete()
+        return redirect('bands')
+    return render(request, 'listings/band_delete.html', {'band' : band})
+
+
+## listing
+### read all
 def listings(request):
     listings = Listing.objects.all()
     return render(request, "listings/listings.html", {"listings" : listings})
 
+### read one
 def listing_details(request, id):
     listing = Listing.objects.get(id=id)
     return render(request, "listings/listing_details.html", {"listing" : listing})
 
+### create
 def listing_create(request):
     if request.method == "POST":
         form = ListingForm(request.POST)
@@ -50,6 +69,7 @@ def listing_create(request):
         form = ListingForm()
     return render(request, 'listings/listing_create.html', {'form' : form})
 
+### update
 def listing_update(request, id):
     listing = Listing.objects.get(id=id)
     if request.method == "POST":
@@ -61,12 +81,23 @@ def listing_update(request, id):
         form = ListingForm(instance=listing)
     return render(request, 'listings/listing_update.html', {'form' : form})
 
+### delete
+def listing_delete(request, id):
+    listing = Listing.objects.get(id=id)
+    if request.method == "POST":
+        listing.delete()
+        return redirect('listings')
+    return render(request, 'listings/listing_delete.html', {'listing' : listing})
+
+# vue about
 def about_us(request):
     return render(request, "listings/about_us.html")
 
+# vue confirmation d'envoi de mail
 def email_sent(request):
     return render(request, 'listings/email_sent.html')
 
+#vue contact
 def contact_us(request):
     if request.method == "POST":
         form = ContactUsForm(request.POST)
